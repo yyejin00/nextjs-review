e8c2d147287a2257e791cb845f6b7a83a7c9b66c
yyejin00 <bywlslgnsl20@gmail.com>
2026-02-22 17:12:34 +0900

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import js from '@eslint/js';
 
 const eslintConfig = defineConfig([
+  js.configs.recommended, //!
   ...nextVitals,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
