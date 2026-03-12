0075d1bf141dfe4d5b6a5fdf5305c539268cb227
yyejin00 <bywlslgnsl20@gmail.com>
2026-03-12 18:16:43 +0900

    chore: 생각주석

--- src/apis/commetsApi.js
+++ src/apis/commetsApi.js
@@ -6,6 +6,11 @@ const toCommentListResponse = (payload) => ({
   list: toSafeList(payload),
   nextCursor: toSafeNextCursor(payload),
 });
+//댓글에는 검증 함가 없는 이유
+//아티클의 경우 articles/123 처럼 브라우저에서 접근이 가능하지만,
+//댓글같은경우에는 주소로 접근하지는 않는다. api를 보낼 때나 해당 명세에 맞게 보내는거지.
+
+//하지만 좀더 꼼꼼하게하려면 댓글도 검증하면 좋다!
 
 export async function fetchArticleCommentList(
   { articleId, limit = 10, cursor } = {},
